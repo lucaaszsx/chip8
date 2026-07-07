1f2cefcd0ddabee558aae183c56a3142c53e84ea
Lucas <jlucasmelob558@gmail.com>
2026-07-07 12:12:56 -0300

    add documentation comments to some methods

--- src/chip8_isa.c
+++ src/chip8_isa.c
@@ -30,18 +30,45 @@ void chip8_isa_sprite(struct Chip8 *chip, uint16_t opcode) {
 }
 
 // internals
+
+/** 
+ * Take the second part of the opcode hexadecimal value
+ * Used to lookup for some register between V0..VF
+ * e.g.: 0x610A -> 0x1 (1)
+ */
 uint8_t chip8_X(uint16_t opcode) {
     return (opcode & 0x0f00) >> 8;
 }
 
+/** 
+ * Take the third part of the opcode hexadecimal value
+ * Also used to lookup for registers
+ */
 uint8_t chip8_Y(uint16_t opcode) {
     return (opcode & 0x00f0) >> 4;
 }
 
+/** 
+ * An 4 bit number formed by the fourth part of the opcode hexadecimal value
+ * e.g.: 0x610A -> 0xA (10)
+ */
+uint8_t chip8_N(uint16_t opcode) {
+    return opcode & 0x000f;
+}
+
+/** 
+ * An 8 bit number formed by the third and fourth parts of the opcode hexadecimal value
+ * e.g.: 0x601A -> 0x1A (26)
+ */
 uint8_t chip8_NN(uint16_t opcode) {
     return opcode & 0x00ff;
 }
 
+
+/** 
+ * An 12 bit number formed by the second, third and fourth parts of the opcode hexadecimal value
+ * e.g.: 0x1200 -> 0x200 (512)
+ */
 uint16_t chip8_NNN(uint16_t opcode) {
     return opcode & 0x0fff;
 }
