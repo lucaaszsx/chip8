caa406ef8ff420afea66fa331e6462931c33de20
Lucas <jlucasmelob558@gmail.com>
2026-07-26 13:13:26 -0300

    feat(asm): add symbol table management

---
+++ casm/include/symbol.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum {
+    SYMBOL_LABEL,
+    SYMBOL_EQU
+} SymbolType;
+
+typedef struct {
+    const char *name;
+    SymbolType type;
+    uint16_t address;
+} Symbol;
+
+typedef struct {
+    Symbol *symbols;
+    size_t count;
+    size_t cap;
+} SymbolTable;
+
+void symbol_tinit(SymbolTable *);
+void symbol_tfree(SymbolTable *);
+bool symbol_create(SymbolTable *, SymbolType type, const char *name, uint16_t address);
+Symbol *symbol_lookup(SymbolTable *, const char *name);
---
+++ casm/src/symbol.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include "symbol.h"
+
+/* initial quantity to be allocated for symbols */
+#define SYMBOL_TABLE_ICAP 16
+
+void symbol_tinit(SymbolTable *table) {
+    table->cap = SYMBOL_TABLE_ICAP;
+    table->count = 0;
+    table->symbols = malloc(table->cap * sizeof(Symbol));
+}
+
+void symbol_tfree(SymbolTable *table) {
+    free(table->symbols);
+
+    table->symbols = NULL;
+    table->cap = 0;
+    table->count = 0;
+}
+
+bool symbol_create(SymbolTable *table, SymbolType type, const char *name, uint16_t address) {
+    if (symbol_lookup(table, name) != NULL)
+        return false;
+
+    if (table->count == table->cap) {
+        table->cap *= 2;
+
+        Symbol *new_symbols = realloc(table->symbols, table->cap * sizeof(Symbol));
+        if (new_symbols == NULL) return false;
+
+        table->symbols = new_symbols;
+    }
+
+    table->symbols[table->count++] = (Symbol){
+        .name=name,
+        .type=type,
+        .address=address
+    };
+
+    return true;
+}
+
+Symbol *symbol_lookup(SymbolTable *table, const char *name) {
+    for (size_t i = 0; i < table->count; i++) {
+        Symbol *sym = &table->symbols[i];
+        if (strcmp(sym->name, name) == 0)
+            return sym;
+    }
+    return NULL;
+}
