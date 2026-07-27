de5d743645ed31f3b4df47f376748b9c325cbccb
Lucas <jlucasmelob558@gmail.com>
2026-07-27 01:49:55 -0300

    chore: xmalloc/xrealloc implementation

--- casm/src/util.c
+++ casm/src/util.c
@@ -1,5 +1,25 @@
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "util.h"
+
+void *xmalloc(size_t size) {
+    void *p = malloc(size);
+    if (p == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    return p;
+}
+
+void *xrealloc(void *ptr, size_t size) {
+    void *p = realloc(ptr, size);
+    if (p == NULL) {
+        perror("realloc");
+        exit(EXIT_FAILURE);
+    }
+    return p;
+}
 
 bool istrcasecmp(const char *s1, const char *s2) {
     while (*s1 && (tolower((unsigned char)*s1) == tolower((unsigned char)*s2))) {
