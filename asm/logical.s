d9d9b2368409d4ac8e3f333a67dfcc5c5e4f8335
Lucas <jlucasmelob558@gmail.com>
2026-07-29 12:48:29 -0300

    chore(asm): add logical operations

--- asm/logical.s
+++ asm/logical.s
@@ -6,23 +6,64 @@ start:
 end:
     jmp end
 
-lg_not:
+; invert bits of A
+;   inputs: v0 (A)
+;   output: v2
+lnot:
+    mov v2, v0   ; copy v0 (A) to v2
     mov ve, 0xff ; mask
-    xor v0, v1   ; v0 = v0 ^ v1 -> e.g.: 0b00001010 (0xa) ^ 0b11111111 (0xff) = 0b11110101
+    xor v2, ve   ; v2 = v2 ^ ve
     rts
 
-lg_nand:
-    and v0, v1
-    jsr lg_not
+; do a nand operation with A and B
+;   inputs: v0 (A), v1 (B)
+;   output: v2
+lnand:
+    and v2, v1 ; apply and: A . B
+    jsr lnot   ; invert bits of the "and" operation result
     rts
 
-lg_gt:
+; do a xnor operation with A and B
+;   inputs: v0 (A), v1 (B)
+;   output: v2
+lxnor:
+    xor v2, v1 ; apply xor: A ^ B
+    jsr lnot   ; invert bits of the "xor" operation result
+    rts
 
-lg_gte:
+; check if A is greater than B
+;   inputs: v0 (B), v1 (A)
+;   output: v2
+lgt:
+    mov v2, v0 ; copy v0 (B) to v2
+    jsr lnot   ; invert bits of B
+    and v2, v1 ; apply and: ~B (v2) . A (v1)
+    rts
 
-lg_lt:
+; check if A is greater than or equal to B
+;   inputs: v0 (B), v1 (A)
+;   output: v0
+lgte:
+    mov v2, v0 ; copy v0 (B) to v2
+    jsr lnot   ; invert bits of B
+    or v2, v1  ; apply or: ~B (v2) + A (v1)
+    rts
 
-lg_lte:
+; check if A is less than B
+;   inputs: v0 (A), v1 (B)
+;   output: v0
+llt:
+    jsr lnot   ; invert bits of A
+    and v0, v1 ; apply and: ~A (v0) . B (v1)
+    rts
+
+; check if A is less than or equal to B
+;   inputs: v0 (A), v1 (B)
+;   output: v0
+llte:
+    jsr lnot  ; invert bits of v0 (A)
+    or v0, v1 ; apply or: ~A (v0) + B (v1)
+    rts
 
 check_sprite:
     .db 0x10, 0xa0, 0x40
